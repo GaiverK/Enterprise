64b0cbb036fd0b41e191bebd8fd5b65f559f6104
GaiverKostyan <gaiverkostyan1987@gmail.com>
2019-12-26 10:15:13 +0200

    Code fixes

--- SimpleCompressor/src/com/gaiver/CompressionStrategy.java
+++ SimpleCompressor/src/com/gaiver/CompressionStrategy.java
@@ -1,8 +1,7 @@
 package com.gaiver;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.zip.DeflaterOutputStream;
 
 public interface CompressionStrategy {
-    DeflaterOutputStream compress(OutputStream data) throws IOException;
+    OutputStream compress(OutputStream data) throws IOException;
 }
--- SimpleCompressor/src/com/gaiver/Compressor.java
+++ SimpleCompressor/src/com/gaiver/Compressor.java
@@ -3,7 +3,6 @@ package com.gaiver;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.zip.DeflaterOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -15,14 +14,12 @@ public class Compressor {
     }
 
     public void compress(Path inFile, File output) throws IOException {
-        try(OutputStream ous = new FileOutputStream(output)) {
-            DeflaterOutputStream zs = strategy.compress(ous);
+        try(OutputStream ous = new FileOutputStream(output);OutputStream zs = strategy.compress(ous);) {
             if( output.getName().contains(".zip") ){ // If file contains zip extension then add zip entry
                 ZipOutputStream zos = (ZipOutputStream) zs;
                 zos.putNextEntry(new ZipEntry(inFile.getFileName().toString()));
             }
             Files.copy(inFile, zs);
-            zs.close();
         }
     }
 }
